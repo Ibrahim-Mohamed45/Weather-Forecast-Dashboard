c05e4d417b7d9eaeb4080d82b56c44a4b651a0b7
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-30 17:34:39 +0000

    Add today's forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
 
-    const key = "76d996ed211d97f5cfad38a1e858f70a";
+    const key = "2da6629ff1a77167ddf523563768f9b8";
+
+    let day = dayjs().format('DD MM YYYY');
     
+    var todayForecast ;
+
     $('#search-button').on('click', function(e) {
 
         let cityName = $('#search-input').val();
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
         console.log(queryURL);
 
         e.preventDefault();
@@ -16,9 +20,44 @@ $( document ).ready(function() {
             method: "GET"
         }).then(function(response) {
 
-            console.log(response);
+            todayForecast = response;
+            currentWeather(todayForecast);
+            console.log(todayForecast);
 
         });
 
     })
+
+    function currentWeather() {
+        var todayBox = $("#today");
+        var todayCard = $("<div>");
+        todayCard.attr("class", "card border-dark");
+        todayBox.append(todayCard);
+      
+        var cardBody = $('<div class="card-body">');
+        todayCard.append(cardBody);
+      
+        var cardTitle = $('<h5 class="card-title">');
+        cardTitle.text(`${todayForecast.name} (${day})`);
+
+        var iconcode = todayForecast.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        cardTitle.append(`<img class="ml-3 bg-info rounded-circle " src=${iconurl} alt="${todayForecast.weather[0].description}">`);
+        cardBody.append(cardTitle);
+      
+        var tempText = $('<p class="card-text">');
+        tempText.text(`Temp: ${(todayForecast.main.temp - 273.15).toFixed(2)} ºC`);
+        cardBody.append(tempText);
+      
+        var windText = $('<p class="card-text">');
+        windText.text(`Wind: ${todayForecast.wind.speed}`);
+        cardBody.append(windText);
+      
+        var humidityText = $('<p class="card-text">');
+        humidityText.text(`Humidity: ${todayForecast.main.humidity}%`);
+        cardBody.append(humidityText);
+    }
+
 });
+
