1ec8a1b8aa1a4dc6a040d621c132f651cd4ad6d6
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-02-01 14:30:32 +0000

    Add 5 cards to forecast section that displays avg temp, wind, humidity and the date

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,15 +33,14 @@ $( document ).ready(function() {
     function currentWeather() {
 
         let todayForecast = weatherArr.list[0]
-        var todayBox = $("#today");
         var todayCard = $("<div>");
         todayCard.attr("class", "card border-dark");
-        todayBox.append(todayCard);
+        $("#today").append(todayCard);
       
         var cardBody = $('<div class="card-body">');
         todayCard.append(cardBody);
       
-        var cardTitle = $('<h5 class="card-title">');
+        var cardTitle = $('<h3 class="card-title">');
         cardTitle.text(`${weatherArr.city.name} (${day})`);
 
         var iconcode = todayForecast.weather[0].icon;
@@ -101,35 +100,46 @@ $( document ).ready(function() {
         let day3temp = 0 ; let day3wind = 0; let day3humidity = 0;
         let day4temp = 0 ; let day4wind = 0; let day4humidity = 0;
         let day5temp = 0 ; let day5wind = 0; let day5humidity = 0;
+        let day1Icon; let day2Icon; let day3Icon; let day4Icon; let day5Icon;
 
         for (var i = 0; i < day1.length; i++) {
             day1temp += ((day1[i].main.temp - 273.15).toFixed(2) * 1) / day1.length;
             day1wind += (day1[i].wind.speed * 1) / day1.length;
             day1humidity += (day1[i].main.humidity * 1) / day1.length;
+            let index = Math.round(day1.length/2);
+            day1Icon = day1[index].weather[0].icon;
         }
         
         for (var i = 0; i < day2.length; i++) {
             day2temp += ((day2[i].main.temp - 273.15).toFixed(2) * 1) / day2.length;
             day2wind += (day2[i].wind.speed * 1) / day2.length;
             day2humidity += (day2[i].main.humidity * 1) / day2.length;
+            let index = Math.round(day2.length/2);
+            day2Icon = day2[index].weather[0].icon;
         }
         
         for (var i = 0; i < day3.length; i++) {
             day3temp += ((day3[i].main.temp - 273.15).toFixed(2) * 1) / day3.length;
             day3wind += (day3[i].wind.speed * 1) / day3.length;
             day3humidity += (day3[i].main.humidity * 1) / day3.length;
+            let index = Math.round(day3.length/2);
+            day3Icon = day3[index].weather[0].icon;
         }
 
         for (var i = 0; i < day4.length; i++) {
             day4temp += ((day4[i].main.temp - 273.15).toFixed(2) * 1) / day4.length;
             day4wind += (day4[i].wind.speed * 1) / day4.length;
             day4humidity += (day4[i].main.humidity * 1) / day4.length;
+            let index = Math.round(day4.length/2);
+            day4Icon = day4[index].weather[0].icon;
         }
 
         for (var i = 0; i < day5.length; i++) {
             day5temp += ((day5[i].main.temp - 273.15).toFixed(2) * 1) / day5.length;
             day5wind += (day5[i].wind.speed * 1) / day5.length;
             day5humidity += (day5[i].main.humidity * 1) / day5.length;
+            let index = Math.round(day5.length/2);
+            day5Icon = day5[index].weather[0].icon;
         }
 
         avgTemp=[day1temp.toFixed(2),day2temp.toFixed(2),day3temp.toFixed(2),day4temp.toFixed(2),day5temp.toFixed(2)];
@@ -138,6 +148,37 @@ $( document ).ready(function() {
 
         avgHumidity=[Math.round(day1humidity),Math.round(day2humidity),Math.round(day3humidity),Math.round(day4humidity),Math.round(day5humidity)];
 
+        dates=[a,b,c,d,e];
+
+        icons=[day1Icon, day2Icon, day3Icon, day4Icon, day5Icon];
+
+        console.log(icons);
+
+        let forecastHeader = $(
+        ` <h3 class="px-3 w-100"> 5 Day Forecast: </h3> `);
+        $('#forecast').append(forecastHeader);
+
+        for(var i=0; i<5; i++) {
+
+            var iconurl = "http://openweathermap.org/img/w/" + icons[i] + ".png";
+            
+            let forecastCard = $(
+                `
+                <div class="card m-3 p-3 col-lg-2 col-11 bg-info text-white">
+                <h4 class="card-title p-1">${dates[i]}</h4>
+                <div class="card-body p-2">
+                <div class="pb-3">
+                <img class="ml-2" src=${iconurl}>
+                </div>
+                <p class="card-text">Temp: ${avgTemp[i]} ℃</p>
+                <p class="card-text">Wind: ${avgWind[i]} </p>
+                <p class="card-text">Humidity: ${avgHumidity[i]}%</p>
+                </div>
+                </div>
+                `);
+                
+                $('#forecast').append(forecastCard);
+        }
     }
 
 });
